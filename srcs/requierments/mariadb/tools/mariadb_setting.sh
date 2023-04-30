7bfc93bcb76c7b568bdb82203289397b09ffcbe2
jaeyjeon <jaeyjeon@student.42seoul.kr>
2023-05-01 01:28:03 +0900

    test mariadb

--- srcs/requierments/mariadb/tools/mariadb_setting.sh
+++ srcs/requierments/mariadb/tools/mariadb_setting.sh
@@ -3,10 +3,10 @@
 service mysql start
 
 echo "CREATE DATABASE IF NOT EXISTS wordpress ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY '12345';"  > db1.sql
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'user'@'%';"  > db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';"  > db1.sql
-echo "FLUSH PRIVILEGES;" > db1.sql
+echo "CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY '12345';"  >> db1.sql
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'user'@'%';"  >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';"  >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
 
 mysql < db1.sql
 
