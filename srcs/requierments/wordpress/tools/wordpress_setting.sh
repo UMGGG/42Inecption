b17eb649b3ad4a087c536306d1cacfe5f1004e3f
jaeyjeon <jaeyjeon@student.42seoul.kr>
2023-05-01 03:12:32 +0900

    test mariadb

--- srcs/requierments/wordpress/tools/wordpress_setting.sh
+++ srcs/requierments/wordpress/tools/wordpress_setting.sh
@@ -12,7 +12,7 @@ wp core download --allow-root
 mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 mv /wp-config.php /var/www/html/wp-config.php
 
-wp core install --url=$WP_URL/ --title=$WP_TITLE --admin_user=$WP_ADMIN_LOGIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp core install --path=/var/www/html --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_LOGIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 
 wp user create $WP_USER_LOGIN $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD --allow-root
 
