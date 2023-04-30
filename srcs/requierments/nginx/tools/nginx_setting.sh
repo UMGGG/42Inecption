b5d33e0da235f21eea85a80ba74e9251d920a181
jaeyjeon <jaeyjeon@student.42seoul.kr>
2023-05-01 01:06:07 +0900

    1

--- srcs/requierments/nginx/tools/nginx_setting.sh
+++ srcs/requierments/nginx/tools/nginx_setting.sh
@@ -3,6 +3,6 @@
 echo "Nginx setting...";
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/nginx-selfsigned.key -out /etc/ssl/nginx-selfsigned.crt -subj "/C=KR/ST=Seoul/O=42Seoul/OU=jaeyjeon/CN=Inception/"
 
-mv /default /etc/nginx/sites-available/default
+mv /default /etc/nginx/sites-enabled/default
 
 exec nginx -g "daemon off;"
