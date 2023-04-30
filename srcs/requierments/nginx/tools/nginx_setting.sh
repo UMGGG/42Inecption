9f386e50ee286098e8bbadfb96f8411baa6f038c
jaeyjeon <jaeyjeon@student.42seoul.kr>
2023-04-30 23:52:09 +0900

    fix ngins sh

--- srcs/requierments/nginx/tools/nginx_setting.sh
+++ srcs/requierments/nginx/tools/nginx_setting.sh
@@ -4,9 +4,10 @@ if [ ! -f /etc/ssl/certs/nginx.crt ];
 then
 echo "Nginx setting...";
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/nginx-selfsigned.key -out /etc/ssl/nginx-selfsigned.crt -subj "/C=KR/ST=Seoul/O=42Seoul/OU=jaeyjeon/CN=Inception/"
+chown nginx /etc/ssl/nginx-selfsigned.crt
+chown nginx /etc/ssl/nginx-selfsigned.key
+nginx -g "daemon off;";
 echo "Nginx setting fin";
 fi
-
-echo "127.0.0.1 jaeyjeon.42.fr" >> /etc/hosts
 
 exec "$@"
