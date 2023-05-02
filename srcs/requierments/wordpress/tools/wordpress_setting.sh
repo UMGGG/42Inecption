2ca28d66d2a1accfb7c09587d992a7943b68da10
jaeyjeon <jaeyjeon@student.42seoul.kr>
2023-05-03 05:07:22 +0900

    fix user data

--- srcs/requierments/wordpress/tools/wordpress_setting.sh
+++ srcs/requierments/wordpress/tools/wordpress_setting.sh
@@ -16,7 +16,7 @@ mv /wp-config.php /var/www/html/wp-config.php
 
 wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_LOGIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 
-wp user create $WP_USER_LOGIN $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD --allow-root
+wp user create $WP_USER_LOGIN $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
 fi
 
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
