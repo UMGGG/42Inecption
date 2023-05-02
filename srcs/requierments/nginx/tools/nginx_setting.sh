898739b561c15037602a2a6dd1f14f0f38494783
jaeyjeon <jaeyjeon@student.42seoul.kr>
2023-05-03 07:35:43 +0900

    fix www.conf

--- srcs/requierments/nginx/tools/nginx_setting.sh
+++ srcs/requierments/nginx/tools/nginx_setting.sh
@@ -4,6 +4,9 @@ if [ ! -f /etc/ssl/nginx-selfsigned.crt ]; then
 echo "Nginx setting...";
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/nginx-selfsigned.key -out /etc/ssl/nginx-selfsigned.crt -subj "/C=KR/ST=Seoul/O=42Seoul/OU=jaeyjeon/CN=Inception/"
 
+chown nginx /etc/ssl/nginx-selfsigned.key
+chown nginx /etc/ssl/nginx-selfsigned.crt
+
 # mv /default /etc/nginx/sites-enabled/default
 fi
 
