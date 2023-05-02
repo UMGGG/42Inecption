02847e829ce37096cfdc7b293b85bde7aa19d4e1
jaeyjeon <jaeyjeon@student.42seoul.kr>
2023-05-03 00:14:36 +0900

    add mkdir /var/run

--- srcs/requierments/wordpress/tools/wordpress_setting.sh
+++ srcs/requierments/wordpress/tools/wordpress_setting.sh
@@ -2,7 +2,6 @@
 
 
 mkdir -p /var/www/html
-mkdir -p /var/run/
 
 cd /var/www/html
 if [ ! -f /var/www/html/wp-config.php ]; then
@@ -18,10 +17,10 @@ mv /wp-config.php /var/www/html/wp-config.php
 wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_LOGIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 
 wp user create $WP_USER_LOGIN $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD --allow-root
+fi
 
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
 mkdir /run/php
-fi
 
 /usr/sbin/php-fpm7.3 -R -F
